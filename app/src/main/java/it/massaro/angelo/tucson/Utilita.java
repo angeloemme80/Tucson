14f47a08219b727e4ae47b41a863128875760abe
Angelo <angeloemme80@alice.it>
2016-10-16 20:00:05 +0200

    Progetto Tucson

--- app/src/main/java/it/massaro/angelo/tucson/Utilita.java
+++ app/src/main/java/it/massaro/angelo/tucson/Utilita.java
@@ -19,6 +19,7 @@ package it.massaro.angelo.tucson;
         import java.util.Date;
         import java.util.Locale;
         import java.util.Map;
+        import java.util.TimeZone;
 
 /**
  * Created by admwks on 27/09/2016.
@@ -60,16 +61,18 @@ public class Utilita {
         yyyyMMddHHmmss = yyyyMMddHHmmss.replace(":", "");
         yyyyMMddHHmmss = yyyyMMddHHmmss.replace("/", "");
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
-        String ret = "";
-        String sDate = "";
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        //String sDate = "";
+        String reportDate = "";
         try {
             Date date = formatter.parse(yyyyMMddHHmmss);
-            sDate = DateFormat.format("dd/MM/yyyy HH:mm:ss", date).toString();
+            reportDate = sdf.format(date);
+            //sDate = DateFormat.format("dd/MM/yyyy HH:mm:ss", date).toString();
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        return sDate;
+        return reportDate;
     }
 
 
